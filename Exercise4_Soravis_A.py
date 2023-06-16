372cf658fbd71609ff4ced2b89a4db4ba76323c7
JulienSOR <136337150+JulienSOR@users.noreply.github.com>
2023-06-16 23:13:29 +0700

    Create Exercise4_Soravis_A.py

---
+++ Exercise4_Soravis_A.py
@@ -0,0 +1,9 @@
+FE = 'เป็นล้านเลยหรอพี่'
+GB = 89.5
+ICS = 666.333
+CP = 101
+print('---Your Score---')
+print('Foundation English :',FE)
+print('General Business :้',GB)
+print('Introduction to Computer Systems :',ICS)
+print('Computer Programming :',CP)
